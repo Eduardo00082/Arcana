aeb5afb5a57064b78cca84c65d6755f6d83f8ef5
Eduardo00082 <eduentidade@gmail.com>
2026-02-09 11:56:51 +0000

    Add the starry background file to the app (for testing purposes only).

---
+++ app/starfield-background/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Starfield } from "@/components/starfield";
+
+export default function StarfieldPage() {
+  return (
+    <main className="relative min-h-screen overflow-hidden bg-black">
+      <Starfield />
+    </main>
+  );
+}
